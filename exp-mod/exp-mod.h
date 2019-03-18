53929f46c19caa97ef4ce44b722493e7fc8713ce
Maxilbert <Maxilbert@users.noreply.github.com>
2019-03-18 11:11:58 -0400

    Update exp-mod.h

--- exp-mod/exp-mod.h
+++ exp-mod/exp-mod.h
@@ -3,8 +3,13 @@
 
 #define u32 unsigned int
 
-#define BITS 		 	256
-#define WORD_WIDTH 		32
+#define WORD_WIDTH 		64
+#define NEG_ONE			0xffffffffffffffff
+#define NEG_ONE_OVERFLOW	0x1ffffffffffffffff
+#define SIGN_BIT		0x8000000000000000
+
+#define BITS 		 	2048
+
 #define WORDS 			(BITS / WORD_WIDTH)
 #define WORDS_INFLAT		(BITS / WORD_WIDTH + 2)
 #define WORDS_LONG 		((2*BITS) / WORD_WIDTH)
@@ -39,46 +44,37 @@ struct SuperBigNum {
     u32 num[WORDS_SUPER];
 };
 
-void exp_mod(struct BigNum *, u32, struct BigNum *, struct BigNum *);
-void mul(struct BigNum *, struct BigNum *, struct LongBigNum *);
-void mod(struct LongBigNum *, struct BigNum *, struct BigNum *);
-void div(struct LongBigNum *, struct BigNum *, struct BigNum *);
-void oneBitLeftShift(struct BigNum *);
-int greaterOrEqual(struct BigNum *, struct BigNum *);
-void sub(struct BigNum *, struct BigNum *);
 
-
+int check_keys(struct BigNum *, struct BigNum *, struct BigNum *, u32, u32, struct BigNum *);
 int checkIsTrueReciprocal(struct BigNum *, struct InflatBigNum *);
 void expModByReciprocal(struct BigNum *, u32, struct BigNum *, struct InflatBigNum *, struct BigNum *);
 void sub_new(struct LongBigNum *, struct LongBigNum *, struct BigNum *);
 void modByUsingReciprocal(struct LongBigNum *, struct BigNum *, struct InflatBigNum *, struct BigNum *);
 void divideByMulReciprocal(struct LongBigNum *, struct InflatBigNum *, struct InflatBigNum *);
 void mul_new(struct BigNum *, struct InflatBigNum *, struct LongBigNum *);
-
-
-int check_keys(struct BigNum *, struct BigNum *, struct BigNum *, u32, struct BigNum *);
-
+void mul(struct BigNum *, struct BigNum *, struct LongBigNum *);
+void oneBitLeftShift(struct BigNum *);
 void add(struct BigNum *, struct BigNum *);
-void mod_short(struct BigNum *, struct BigNum *, struct BigNum *);
 void mul_short(struct ShortBigNum *, struct ShortBigNum *, struct BigNum *res);
-void sub_one(struct ShortBigNum *);
+void sub_one_short(struct ShortBigNum *);
 
 struct Input {
-    u32 e;		//PK exp
-    struct BigNum n;
-    struct InflatBigNum nReciprocal;
+    u32 e;				//PK exp
+    struct BigNum n;			// base: p*q
+    struct InflatBigNum nReciprocal; 	//Reciprocal of n
 };
 
 struct NIZKInput {
-    struct ShortBigNum p; //prime p
-    struct ShortBigNum q; //prime q
-    struct BigNum d;	//SK exp
-    struct BigNum b;	//encrypted data
+    u32 t; 				// e * d  = (p-1) * (q-1) * t + 1 
+    struct ShortBigNum p; 		//prime p
+    struct ShortBigNum q; 		//prime q
+    struct BigNum d;			//SK exp
+    struct BigNum b;			//decrypted data
 };
 
 struct Output {
-    int isKeyMatched;	//whether e, b, n
-    struct BigNum res;	//decrypted data
+    int isKeyMatched;		//whether e, b, p, q, n match
+    struct BigNum res;		//encrypted data
 };
 
 void outsource(struct Input *input, struct NIZKInput *nizkinput, struct Output *output);
